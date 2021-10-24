cf52cf1e4741f9c0058d84d468340439c20ae8cf
Jan <mail@jan-schmidt.de>
2021-10-24 09:57:56 +0200

    commit

--- start.py
+++ start.py
@@ -3,15 +3,13 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='', static_folder='frontscript', template_folder="templates")
 @app.route("/")
 def index():
     return render_template("index.html") 
 
-@app.route("/credits")
-def credits():
-    return render_template("credits.html") 
+def results():
+     return render_template("results.html")
 
 if __name__ == "__main__":
     app.run(port=4000)
